754469033ffee609f72e7e381c61b501066c11b8
Sarah Brittle <sgbrittle@gmail.com>
2024-09-20 15:10:18 -0500

    changing files

---
+++ JavaScriptEssentialTraining/.ipynb_checkpoints/crimedftest-checkpoint.py
@@ -0,0 +1,14 @@
+file = '2019_NIBRS_NATIONAL_MASTER_FILE_ENC.txt'
+
+f = open(file, 'r')
+print(f.read())
+    #for x in range(10):
+
+    # This stores a reference to a file stream
+        #print(text)
+
+    # Store all of the text inside a variable called "lines"
+        #lines = text.read()
+
+    # Print the contents of the text file
+        #print(lines)
--- JavaScriptEssentialTraining/Practice/03_07/script.js
+++ JavaScriptEssentialTraining/Practice/03_07/script.js
@@ -7,3 +7,30 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const gardenBasket = {
+    name: "Basket",
+    shovels: {
+        smallShovel: "smallShovel",
+        mediumShovel: "mediumShovel",
+        scoopShovel: "scoopShovel",
+    },
+    tools: {
+        hoe: "hoe",
+        miniRake: "miniRake",
+        scissors: "scissors",
+        twine: "twine",
+    },
+    color: "brown",
+    material: "wicker",
+    seeds: {
+        squash: {
+            zucchini: "zucchini",
+            yellowneck: "yellowneck",
+        },
+        tomato: "tomato",
+        pea: "pea",
+    },
+  };
+
+  console.log(gardenBasket)
